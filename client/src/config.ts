0e32038c87f02537da830265bc1c4d1a0353c7e3
OanhNguyenPM13A <nguyenthikieuoanh948@gmail.com>
2022-12-24 16:49:28 +0700

    deploy serverless

---
+++ client/src/config.ts
@@ -0,0 +1,9 @@
+const apiId = '3mlb8dkrsj'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  // domain: 'dev-nd9990-p4.us.auth0.com',
+  domain: 'dev-l7upfcyhzcnvln1c.us.auth0.com',            // Auth0 domain
+  clientId: 'bVuxWY38WS8bQ5NE6uJFsVgVmrrxpM49',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
